5f8a5dd8bcf91e7122243acdb7617fb53d3c7cf5
Skinny <scott.krabbenhoft02@gmail.com>
2023-02-10 14:19:30 -0600

    Movement System Complete
    Finishing basic functionality of movement system. Villagers can be grabbed, dragged and dropped, and make their way back to a base node once they have hit the ground. Hooking into the system at a later time should be trivial.

--- IslandCurator/Assets/Scripts/GrabVillager.cs
+++ IslandCurator/Assets/Scripts/GrabVillager.cs
@@ -9,11 +9,25 @@ public class GrabVillager : GrabbableItem
     [SerializeField] float _clickScale = 0.8f;
     [SerializeField] float _heldScale = 1.3f;
     [SerializeField] float _interpolationSpeed = 0.5f;
+
+    [Header("Drop Settings")]
+    [SerializeField] float _dropDistance = 3f;
+    [SerializeField] float _fallSpeed = 10f;
+
+    [Header("Shadow Settings")]
+    [SerializeField] SpriteRenderer _shadow = null;
+    [SerializeField] float _baseShadowDistance = -0.25f;
+    [SerializeField] float _baseAlpha = 0.05f;
+    [SerializeField] float _heldAlpha = 0.2f;
+    [SerializeField] float _positionInterpolationSpeed = 10f;
+    [SerializeField] float _alphaInterpolationSpeed = 1.5f;
     
     PlayerPickup _playerHolding = null;
     Navigator _movement = null;
+    Vector3 _fallTarget;
     float _scaleModifier = 1f;
     bool _held = false;
+    bool _falling = false;
     
     void Start()
     {
@@ -35,7 +49,8 @@ public class GrabVillager : GrabbableItem
         _scaleModifier = 1f;
 
         _playerHolding.ClearHeld();
-        _movement.ReturnToBaseNode();
+        _falling = true;
+        _fallTarget = transform.position - new Vector3(0, _dropDistance, 0);
     }
 
     void OnMouseDown()
@@ -56,6 +71,14 @@ public class GrabVillager : GrabbableItem
 
     void Update()
     {
+        if (_falling || _held)
+        {
+            if (_falling)
+            {
+                Fall();
+            }
+            UpdateShadow();
+        }
         UpdateScale();
     }
 
@@ -63,19 +86,63 @@ public class GrabVillager : GrabbableItem
     {
         if (transform.localScale.x > _scaleModifier)
         {
-            float scaleThisFrame = Mathf.Clamp(transform.localScale.x - _interpolationSpeed * Time.deltaTime, _scaleModifier, _heldScale);
-            transform.localScale = new Vector3(scaleThisFrame, scaleThisFrame, transform.localScale.z);
-            if ((transform.localScale.x == _scaleModifier) &&
-                (transform.localScale.y == _scaleModifier) &&
-                (_scaleModifier == _clickScale))
+            if (!_falling)
             {
-                _scaleModifier = _heldScale;
+                float scaleThisFrame = Mathf.Clamp(transform.localScale.x - _interpolationSpeed * Time.deltaTime, _scaleModifier, _heldScale);
+                transform.localScale = new Vector3(scaleThisFrame, scaleThisFrame, transform.localScale.z);
+                if ((transform.localScale.x == _scaleModifier) &&
+                    (transform.localScale.y == _scaleModifier) &&
+                    (_scaleModifier == _clickScale))
+                {
+                    _scaleModifier = _heldScale;
+                }
+            }
+            else if (_falling)
+            {
+                float scaleThisFrame = Mathf.Clamp(1 + Mathf.Abs(transform.position.y - _fallTarget.y), 1, _heldScale);
+                transform.localScale = new Vector3(scaleThisFrame, scaleThisFrame, transform.localScale.z);
             }
         }
         else if (transform.localScale.x < _scaleModifier)
         {
             float scaleThisFrame = Mathf.Clamp(transform.localScale.x + 2 * _interpolationSpeed * Time.deltaTime, _clickScale, _scaleModifier);
             transform.localScale = new Vector3(scaleThisFrame, scaleThisFrame, transform.localScale.z);
+        }
+    }
+
+    void UpdateShadow()
+    {
+        if (_falling && _shadow.color.a > _baseAlpha)
+        {
+            float alphaThisFrame = Mathf.Clamp(_shadow.color.a - _alphaInterpolationSpeed * Time.deltaTime, _baseAlpha, _heldAlpha);
+            _shadow.color = new Color(_shadow.color.r, _shadow.color.g, _shadow.color.b, alphaThisFrame);
+        }
+        else if (_held && _shadow.color.a < _heldAlpha)
+        {
+            float alphaThisFrame = Mathf.Clamp(_shadow.color.a + _alphaInterpolationSpeed * Time.deltaTime, _baseAlpha, _heldAlpha);
+            _shadow.color = new Color(_shadow.color.r, _shadow.color.g, _shadow.color.b, alphaThisFrame);
+        }
+
+        if (_held && _shadow.transform.localPosition.y != _baseShadowDistance - _dropDistance)
+        {
+            float yThisFrame = Mathf.Clamp(_shadow.transform.localPosition.y - _positionInterpolationSpeed * Time.deltaTime, _baseShadowDistance - _dropDistance, _baseShadowDistance);
+            _shadow.transform.localPosition = new Vector3(0, yThisFrame, _shadow.transform.localPosition.z);
+        }
+        else if (_falling && _shadow.transform.localPosition.y != _baseShadowDistance)
+        {
+            float yThisFrame = Mathf.Clamp(_shadow.transform.localPosition.y + _fallSpeed * Time.deltaTime, _baseShadowDistance - _dropDistance, _baseShadowDistance);
+            _shadow.transform.localPosition = new Vector3(0, yThisFrame, _shadow.transform.localPosition.z);
+        }
+    }
+
+    void Fall()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _fallTarget, _fallSpeed * Time.deltaTime);
+        
+        if (transform.position == _fallTarget)
+        {
+            _falling = false;
+            _movement.ReturnToBaseNode();
         }
     }
 }
--- README.txt
+++ README.txt
@@ -1,1 +1,92 @@
-### Island Curator (Working Title) ###
+##### Island Curator (Working Title) #####
+
+    The player takes the control of a benevolent deity watching over a secluded tropical isle of tribal villagers. The player must manage their villagers between certain tasks to keep them alive and successful.
+    Each task meets one or more needs of the tribe, but can take resources. Any task requires villagers assigned to it to be productive.
+    - Farming earns grain for villagers to eat.
+    - Hunting finds meat for villagers to eat. Over-hunting causes available meat to diminish
+    - Building creates structures for villagers to inhabit, but expends wood.
+    - Gathering finds wood for structures to be built with. Over-gathering causes available wood to diminish.
+    - Fishing finds fish for villagers to eat. Over-fishing does not cause available fish to diminish, but gives less fish than hunting gives meat. Therefore, a player can use fishing as a fallback when they over-hunt or put all resources in fishing instead of hunting to offset the lower reward.
+    Villagers have a number of needs that need to be met. If a villager’s needs are not being met, they may die.
+    - Villagers need to eat grain
+    - Villagers need to eat either meat or fish
+    - Villagers need to have a house
+    Villagers will periodically spawn, requiring the player to keep up with increasing needs. While villagers’ needs are met, the villagers will continuously build a totem pole in their village. Villagers work slightly faster when in groups, though not enough to offset demands of rising population.
+    Villagers with unmet needs and villager deaths reduce the village’s overall happiness. If the village’s happiness falls too low, the villagers will revolt, destroying the totem pole. When this occurs, the volcano at the center of the island will erupt, throwing out fireballs that will kill villagers where they land. The taller the totem pole is when this occurs, the more fierce the eruption is.
+    Alternatively, players can throw individual villagers into the volcano to create a targeted fireball as a means of population control to reduce production requirements. Deliberately sacrificing villagers, however, reduces village happiness. This keeps the player balancing between revolt and population control.
+    The player’s goal is simply to survive as long as they can. A counter of days survived and maximum totem pole height is kept to gauge player performance. The only way to lose is if all villagers die. To keep pressure on the player, villagers should slowly decrease in productivity as total villager count increases, creating greater chaos at higher populations.
+
+### TO DO ###
+- SCRIPTING
+    - Villager Pickup
+        - Finish implementing villager drop functionality
+    - Villager Sprite
+    - Villager Manager
+        - Resource Manager
+        - Villager Counter
+        - Happiness Tracker
+    - Resource Producer
+    - Implement Villager work assignment functionality
+- VISUALS
+    - Villager Sprite
+    - Villager Animations
+	- Idle
+	- Wiggle
+	- Walk/Run
+	- Hunt
+	- Fish
+	- Build
+	- Farm
+	- Riot
+    - Villager Tools
+	- Spear
+	- Fishing Rod
+	- Hammer
+	- Axe
+	- Hoe
+	- Torch
+	- Pitchfork
+	- Club
+    - Buildings
+        - Huts
+	- Scaffolding
+	- Harbor
+	- Boat
+	- Farm
+	- Village Center
+    - Plants
+	- Trees
+	- Bushes
+    - Crops
+	- Yellow Grain
+        - Green Sprouts
+    - Rocks
+    - Totem Pole Segments
+    - Volcano
+    - Island Base
+    - Fireball
+    - Icons
+	- Fish
+	- Grain
+	- Meat
+	- Wood
+	- Skull
+	- Happy Face
+	- Angry Face
+    - Animals
+	- Fish
+	- Boar
+	- Bird
+- AUDIO
+    - Ambience
+        - Water
+        - Birds
+        - Wind
+    - SFX
+	- Construction Noise
+	- Pig Grunt
+	- Foliage Rustle
+	- Rumble
+	- Lava Bubble
+	- Eruption
+	- Explosion
